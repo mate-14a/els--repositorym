83cda865f5b1ed0c1ecce485ac93f9a05060f36f
Csabányi Máté <Csabnyi.Mt@wattay.local>
2024-01-04 13:54:11 +0100

    na ezt már pusholom

--- main.py
+++ main.py
@@ -3,5 +3,7 @@ if __name__ == '__main__':
     print("legyen zen e legyen tánc")
     print("folytatás kódja")
     print("ez csak egy commit lesz. de nemk push-olom")
+    print("na majd most pusholom majd fel")
     
+
 
